d6eb3a2b0baf0439af76051e1bebcfa77957a421
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-11 10:41:24 +0900

    Handle pagination

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -15,22 +15,37 @@
 import PostList from '@/components/post-list.vue'
 import axios from 'axios'
 
+async function fetchPosts(page) {
+  const response = await axios.get(`http://wp-rest-api.localhost/wp-json/wp/v2/posts?per_page=12&page=${page}`);
+  return {
+    posts: response.data,
+    totalPages: parseInt(response.headers['x-wp-totalpages']),
+    page,
+  };
+}
+
 export default {
   async asyncData({ query }) {
-    const page = query.page || 1;
-    const response = await axios.get(`http://wp-rest-api.localhost/wp-json/wp/v2/posts?per_page=12&page=${page}`);
-    return {
-      posts: response.data,
-      totalPages: parseInt(response.headers['x-wp-totalpages']),
-      page,
-    };
+    const page = parseInt(query.page || 1);
+    return await fetchPosts(page);
   },
   components: {
     PostList,
   },
+  watch: {
+    async page(value) {
+      const data = await fetchPosts(value);
+      Object.assign(this, data);
+    },
+  },
   methods: {
     input(value) {
-      console.log(value); // TODO watch page and translate to valued page
+      this.$router.push({
+        path: 'posts',
+        query: {
+          page: value,
+        },
+      });
     }
   }
 }
