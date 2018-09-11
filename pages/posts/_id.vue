f48a8296ef1571a42af1b7a59470f04708a3872d
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-11 18:50:06 +0900

    Handle post comment

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -2,8 +2,12 @@
   <v-container class="article">
     <h1>{{ post.title.rendered }}</h1>
     <div v-html="post.content.rendered"></div>
-
-    <h2>コメント</h2>
+    <div class="comment-heading">
+      <h2>コメント</h2>
+      <v-btn class="comment-heading-btn" @click="dialog = true">
+        コメントを書く
+      </v-btn>
+    </div>
     <div class="comments-list">
       <v-card v-for="comment of comments" :key="comment.id">
         <v-card-text>
@@ -14,7 +18,40 @@
       </v-card>
     </div>
 
-    <button @click="postComment">post</button>
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <v-card>
+        <v-card-text>
+          <v-form
+            v-model="valid"
+            ref="form"
+            @submit.prevent="postComment"
+          >
+            <v-text-field
+              v-model="form.author_name"
+              :rules="rules.author_name"
+              label="Name"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="form.author_email"
+              :rules="rules.author_email"
+              label="Email"
+              required
+            ></v-text-field>
+            <v-textarea
+              v-model="form.content"
+              :rules="rules.content"
+              label="Content"
+              required
+            ></v-textarea>
+            <v-btn type="submit" :disabled="!valid">送信</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -49,19 +86,65 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      dialog: false,
+      valid: false,
+      form: {
+        content: '',
+        author_name: '',
+        author_email: '',
+      },
+      rules: {
+        content: [
+          v => !!v || '内容は必須項目です'
+        ],
+        author_name: [
+          v => !!v || '名前は必須項目です'
+        ],
+        author_email: [
+          v => !!v || 'メールアドレスは必須項目です',
+          v => /.+@.+/.test(v) || 'メールアドレスの形式が正しくありません'
+        ],
+      },
+    }
+  },
   created() {
     console.log(this.post);
     console.log('---')
     console.log(this.comments)
   },
   methods: {
-    postComment() {
-      axios.post('http://wp-rest-api.localhost/wp-json/wp/v2/comments/', {
+    async postComment() {
+      if (!this.$refs.form.validate()) {
+        return;
+      }
+      const {
+        content,
+        author_name,
+        author_email,
+      } = this.form;
+      const response = await axios.post('http://wp-rest-api.localhost/wp-json/wp/v2/comments/', {
         post: this.$route.params.id,
-        content: 'lorem ipsum dolor sit amet!',
-        author_name: 'Me!',
-        author_email: 'r-endo@studio-spoon.co.jp',
-      }).catch(e => console.log(e.response));
+        content,
+        author_name,
+        author_email,
+      })
+        .catch(e => console.log(e.response))
+        .then(response => {
+          const { data } = response;
+          if (data.status === 'approved') {
+            this.comments.splice(0, 0, data);
+          } else {
+            console.log('承認待ちコメントを受け付けました。')
+          }
+
+          this.clear();
+          this.dialog = false;
+        });
+    },
+    clear() {
+      this.$refs.form.reset();
     }
   },
   filters: {
@@ -81,6 +164,17 @@ export default {
 
 .comments-list > *:not(:last-child) {
   margin-bottom: 8px;
+}
+
+.comment-heading {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 15px;
+}
+
+.comment-heading-btn {
+  margin: 0;
 }
 
 </style>
