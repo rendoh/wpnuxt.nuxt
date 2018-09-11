235df997f65fb03b3d4c20e9ea4fc546280e9130
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-11 11:06:13 +0900

    Add toolbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
   <v-app>
-    <nuxt/>
+    <v-toolbar color="primary" dark fixed>
+      <v-toolbar-title>
+        <nuxt-link class="home" to="/">WPNuxt</nuxt-link>
+      </v-toolbar-title>
+    </v-toolbar>
+    <div class="content">
+      <nuxt />
+    </div>
   </v-app>
 </template>
+
+<style scoped>
+.content {
+  padding-top: 64px;
+}
+.home {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
+
