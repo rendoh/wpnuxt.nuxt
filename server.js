a63ea7bf01be2371b6c19da1233d5b8812b801f6
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-09-19 15:12:11 +0900

    Update yml

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express')
+const express = require('express');
 const { Nuxt } = require('nuxt')
 const awsServerlessExpress = require('aws-serverless-express')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 
 const app = express()
 const config = require('./nuxt.config')
@@ -9,9 +10,11 @@ config.mode = 'universal'
 const nuxt = new Nuxt(config)
 
 app.use(nuxt.render)
+app.use(awsServerlessExpressMiddleware.eventContext())
 
 const server = awsServerlessExpress.createServer(app)
 
 exports.render = (event, context) => {
   awsServerlessExpress.proxy(server, event, context)
-}
+};
+
